61530b436266699d2d7911f53972a13d199f53d9
Denis Onder <denis_onder@protonmail.com>
2019-12-26 01:08:45 +0100

    Added custom font and tweaked  the docs JS script to execute on window laod

--- public/js/docs.js
+++ public/js/docs.js
@@ -7,11 +7,15 @@ function checkForTabEntities(elem) {
 }
 
 function addLinkToNavbar({ id, innerText: text }) {
-  navbar.innerHTML += `<li><a href="#${id}">> ${text.replace(
+  navbar.innerHTML += `<li><a href="#${id}"> > ${text.replace(
     ":",
     ""
   )}</a></li>`;
 }
 
-Array.from(document.getElementsByTagName("code")).forEach(checkForTabEntities);
-Array.from(document.getElementsByTagName("h3")).forEach(addLinkToNavbar);
+window.onload = () => {
+  Array.from(document.getElementsByTagName("code")).forEach(
+    checkForTabEntities
+  );
+  Array.from(document.getElementsByTagName("h3")).forEach(addLinkToNavbar);
+};
