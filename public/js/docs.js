88d3bdc71b3c340aba05713ef886f66266b8f77d
Denis Onder <denis_onder@protonmail.com>
2019-12-27 16:08:55 +0100

    Vim navigation keys for docs nav links

--- public/js/docs.js
+++ public/js/docs.js
@@ -1,4 +1,6 @@
 const navbar = document.getElementById("docs_nav_list");
+const links = document.getElementsByClassName("docs_nav_link");
+let navCounter = 1; // The counter starts at '1' so it can skip the 'Go Back' link
 
 function checkForTabEntities(elem) {
   const query = "&nbsp;&nbsp;";
@@ -9,13 +11,25 @@ function checkForTabEntities(elem) {
 function addLinkToNavbar({ id, innerText: text, tagName }) {
   navbar.innerHTML += `<li ${
     tagName === "H5" ? `style="margin: 0"` : false
-  }><a href="#${id}"> ${tagName === "H5" ? ">" : ""} ${text.replace(
-    ":",
-    ""
-  )}</a></li>`;
+  }><a class="docs_nav_link" href="#${id}"> ${
+    tagName === "H5" ? ">" : ""
+  } ${text.replace(":", "")}</a></li>`;
+}
+
+function vimNavigation({ keyCode }) {
+  if (keyCode === 75) {
+    // 'J' key handler -> Move down
+    if (navCounter > 0) navCounter--;
+    links[navCounter].focus();
+  } else if (keyCode === 74) {
+    // 'K' key handler -> Move up
+    if (navCounter < links.length - 1) navCounter++;
+    links[navCounter].focus();
+  }
 }
 
 window.onload = () => {
+  this.addEventListener("keydown", vimNavigation);
   Array.from(document.getElementsByTagName("code")).forEach(
     checkForTabEntities
   );
