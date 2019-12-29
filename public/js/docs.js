0d3bf29ba6c3f5429a917298a615056a4c6cb0c9
Denis Onder <denis_onder@protonmail.com>
2019-12-29 02:57:24 +0100

    Writing some comments

--- public/js/docs.js
+++ public/js/docs.js
@@ -2,13 +2,16 @@ const navbar = document.getElementById("docs_nav_list");
 const links = document.getElementsByClassName("docs_nav_link");
 let navCounter = 1; // The counter starts at '1' so it can skip the 'Go Back' link
 
+// Remove the HTML entities from the Markdown generated code blocks
 function checkForTabEntities(elem) {
   const query = "&nbsp;&nbsp;";
   if (elem.innerText.includes(query))
     elem.innerHTML = elem.innerText.split(query).join("  ");
 }
 
+// Generate anchor links for each "section" of the documentation
 function addLinkToNavbar({ id, innerText: text, tagName }) {
+  // H3 and H5 tags are used in the Markdown file to signify the beginning of a section
   navbar.innerHTML += `<li ${
     tagName === "H5" ? `style="margin: 0"` : false
   }><a class="docs_nav_link" href="#${id}"> ${
@@ -16,15 +19,20 @@ function addLinkToNavbar({ id, innerText: text, tagName }) {
   } ${text.replace(":", "")}</a></li>`;
 }
 
-function vimNavigation({ keyCode }) {
-  if (keyCode === 75) {
-    // 'J' key handler -> Move down
-    if (navCounter > 0) navCounter--;
-    links[navCounter].focus();
-  } else if (keyCode === 74) {
-    // 'K' key handler -> Move up
-    if (navCounter < links.length - 1) navCounter++;
-    links[navCounter].focus();
+// Integrate Vim style navigation for the navigation bar
+function vimNavigation({ keyCode: key }) {
+  // 74 => J || 75 => K
+  if (key === 74 || key === 75) {
+    switch (key) {
+      case 74: // J => Move the focus down the navigation bar
+        if (navCounter < links.length - 1) navCounter++;
+        links[navCounter].focus();
+        break;
+      case 75: // K => Move the focus up the navigation bar
+        if (navCounter > 0) navCounter--;
+        links[navCounter].focus();
+        break;
+    }
   }
 }
 
