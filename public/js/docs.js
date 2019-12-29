0e4dd538dc08429cfb6ac24e6bc8e1603f1d3dc9
Denis Onder <denis_onder@protonmail.com>
2019-12-29 04:17:56 +0100

    Grouping links

--- public/js/docs.js
+++ public/js/docs.js
@@ -1,5 +1,6 @@
 const navbar = document.getElementById("docs_nav_list");
 const links = document.getElementsByClassName("docs_nav_link");
+const headers = document.querySelectorAll("h3,h5");
 let navCounter = 1; // The counter starts at '1' so it can skip the 'Go Back' link
 
 // Remove the HTML entities from the Markdown generated code blocks
@@ -9,14 +10,43 @@ function checkForTabEntities(elem) {
     elem.innerHTML = elem.innerText.split(query).join("  ");
 }
 
-// Generate anchor links for each "section" of the documentation
-function addLinkToNavbar({ id, innerText: text, tagName }) {
-  // H3 and H5 tags are used in the Markdown file to signify the beginning of a section
-  navbar.innerHTML += `<li ${
+// Generate link markup
+function linkMarkupGenerator({ id, innerText: text, tagName }) {
+  return `<li ${
     tagName === "H5" ? `style="margin: 0"` : false
   }><a class="docs_nav_link" href="#${id}"> ${
     tagName === "H5" ? ">" : ""
   } ${text.replace(":", "")}</a></li>`;
+}
+
+// Render link groups
+function renderLinkGroup(links) {
+  let groupDiv = document.createElement("div");
+  groupDiv.className = "docs_nav_link_group";
+  links.forEach(link => (groupDiv.innerHTML += link));
+  navbar.appendChild(groupDiv);
+}
+
+// Generate link groups
+function groupLinks(start) {
+  const arr = Array.from(document.querySelectorAll("h3,h5"));
+  const group = arr.slice(start, arr.length);
+  // Add previous element to output array, since that is the H3
+  const prev = group[0];
+  let output = [linkMarkupGenerator(prev)];
+  for (let i = 1; i < group.length; i++) {
+    const current = group[i];
+    if (current.tagName === "H3") break;
+    output.push(linkMarkupGenerator(current));
+  }
+  renderLinkGroup(output);
+}
+
+// Generate anchor links for each "section" of the documentation
+function addLinkToNavbar(e, i) {
+  // H3 and H5 tags are used in the Markdown file to signify the beginning of a section
+  if (e.tagName === "H3") groupLinks(i);
+  // navbar.innerHTML += linkMarkupGenerator(e);
 }
 
 // Integrate Vim style navigation for the navigation bar
