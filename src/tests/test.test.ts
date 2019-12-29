abfa451c552d497c683f31299f0976f3fef1baa4
Denis Onder <denis_onder@protonmail.com>
2019-12-29 17:50:53 +0100

    Added script hooks

--- src/tests/test.test.ts
+++ src/tests/test.test.ts
@@ -1,19 +1,5 @@
 import { expect } from "chai";
 
-/** Rules for automating server startup
- *  import server from "../Server";
- *
- *  before(() => server.start());
- *
- *  afterEach(function() {
- *  if (this.currentTest.state === "failed") {
- *    server.stop("Test suite failed.");
- *  }
- *  });
- *
- *  after(() => server.stop());
- */
-
 describe("Example test", () => {
   it("should return 2", () => {
     expect(1 + 1).to.eq(2);
